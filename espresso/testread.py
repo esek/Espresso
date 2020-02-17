3312b5060ee38cc33a5b6b569deef6f9d1a90d04
Ludvig Lifting <lu8023li-s@student.lu.se>
2020-02-17 19:54:38 +0000

    Improved test script.
    structuring and changing code to be fully functional. Saving card numbers to dict.
    Getting AUTH ERROR while scanning card, but it works, doesn't seem to effect anything

--- espresso/testread.py
+++ espresso/testread.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 
+#Fungerande kod <3
+
 import RPi.GPIO as GPIO
 import time
 
+#Variables n stuff
+i = 0
 from mfrc522 import SimpleMFRC522
-
 reader = SimpleMFRC522()
+GPIO.cleanup()
+values = {}
 
+#loop
 while True:
-
-	
+		
+	print(i)
 	try:
 		id, text = reader.read()
 		print(id)
-		#print(text)
+		values[i] = id
+	#	print(text)
+	except Exception as e:
+		print(e)
 	finally:
-		GPIO.cleanup()
+		print("cleanup")
+		#GPIO.cleanup()
 	time.sleep(1)
+	print("loop")
+	print(values)
+	i += 1
