c1101162f476b9c3e86b04c8596675383f73bdae
ludvig <9506luli@gmail.com>
2020-02-24 20:22:10 +0100

    nfc handler done

--- espresso/nfc_handler.py
+++ espresso/nfc_handler.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python
 
-#Fungerande kod <3
+def nfc_reader():
+	import RPi.GPIO as GPIO
+	import time
 
-import RPi.GPIO as GPIO
-import time
+	timeout = 1800 # (s)
+	counter = 0 #(s)
+	from mfrc522 import SimpleMFRC522
+	reader = SimpleMFRC522() #define object reader to read
+	GPIO.cleanup() 
+	value = 0
 
-#Variables n stuff
-i = 0
-from mfrc522 import SimpleMFRC522
-reader = SimpleMFRC522()
-GPIO.cleanup()
-values = {}
-
-#loop
-while True:
-		
-	print(i)
-	try:
-		id, text = reader.read()
-		print(id)
-		values[i] = id
-	#	print(text)
-	except Exception as e:
-		print(e)
-	finally:
-		print("cleanup")
-		#GPIO.cleanup()
-	time.sleep(1)
-	print("loop")
-	print(values)
-	i += 1
+	#loop for reading nfc reader
+	while counter < timeout:	
+		try:
+			id = reader.read() #read the nfc reader into variable "id"
+			value = id	 
+		except Exception as e:
+			#prints exception
+			print(e) 
+		finally:
+			time.sleep(1) #1s delay for timeout
+			counter += 1 #increment counter for timeout
+			if value != 0:	
+				return value
